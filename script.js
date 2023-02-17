9d2f6364e3e29eeeee5305de84aaca9ab597e3ea
Diego Hernandez <diegoalejandro061292@gmail.com>
2023-02-16 21:51:04 -0500

    1

--- script.js
+++ script.js
@@ -17,7 +17,11 @@ imprimirNombre() {
 const producto1= new Producto("Lavatrastes", 200, 32434, 's');
 
 const producto2= new Producto("lavadora", 200, .32434, 's');
+const producto3= new Producto("salchicha", 200, .32434, 's');
+
 
 console.log(producto1);
 producto1.imprimirNombre();
 producto2.imprimirNombre();
+
+producto3.fechaDeCaducidad;
